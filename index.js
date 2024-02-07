493eda9c72c05a2e0b5d7da4e0e6033e5bd0a277
oolongraccoon <133898805+oolongraccoon@users.noreply.github.com>
2024-02-07 08:54:53 +0800

    backend

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Person = require('./models/person')
 app.use(express.static('build'))
 app.use(cors())
 morgan.token('req-body', req => JSON.stringify(req.body))
-const Format = ':method :url :status :res[content-length] - :response-time ms :req-body';
+const Format = ':method :url :status :res[content-length] - :response-time ms :req-body'
 app.use(morgan(Format))
 app.use(express.json())
 
@@ -29,34 +29,34 @@ app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
-          response.json(person)
+        response.json(person)
       } else {
-          response.status(404).end()
+        response.status(404).end()
       }
     })
     .catch(error => {
       console.error(error)
       next(error)
-    })  
+    })
 })
 app.delete('/api/persons/:id', (request, response, next) => {
 
   Person.findByIdAndDelete(request.params.id)
-    .then(person => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
 })
 
-  
+
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
   if (body.name === undefined) {
     return response.status(400).json({ error: 'content missing' })
-  }  
+  }
   if (body.number === undefined) {
     return response.status(400).json({ error: 'content missing' })
-  } 
+  }
 
   const person = new Person ({
     name: body.name,
@@ -75,22 +75,22 @@ app.put('/api/persons/:id', (request, response, next) => {
   const { name,number } = request.body
   Person.findByIdAndUpdate(request.params.id,request.body,
     { new: true, runValidators: true, context: 'query' })
-  .then(updatedPerson => {
-    response.json(updatedPerson)
-  })
-  .catch(error => next(error))
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 
 app.get('/info', (request, response, next) => {
   Person.countDocuments({})
-  .then(people => {
-    const info = `Phonebook has info for ${people} people.
+    .then(people => {
+      const info = `Phonebook has info for ${people} people.
     <p></p>
     ${new Date()}`
-    response.send(info)
-  })
-  .catch(error => next(error))
+      response.send(info)
+    })
+    .catch(error => next(error))
 
 })
 const unknownEndpoint = (request, response) => {
@@ -103,10 +103,10 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  }
   else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
-  } 
+  }
   else {
     return response.status(500).send({ error: 'Internal Server Error' })
   }
@@ -118,5 +118,5 @@ const errorHandler = (error, request, response, next) => {
 app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
